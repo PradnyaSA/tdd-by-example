97aa201887f5fd8a6f7f7874df4f2a6380881fa6
pradnya.adwankar <pradnya.adwankar@gmail.com>
2024-10-17 18:04:52 -0700

    rename application

--- src/main/java/com/experiment/cafe/tddbyexample/CafeMyBrewApplication.java
+++ src/main/java/com/experiment/cafe/tddbyexample/CafeMyBrewApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TddByExampleApplication {
+public class CafeMyBrewApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TddByExampleApplication.class, args);
+        SpringApplication.run(CafeMyBrewApplication.class, args);
     }
 
 }
